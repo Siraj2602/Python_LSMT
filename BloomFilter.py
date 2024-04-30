46ca79d4f125de648dd9128562a48087903b7334
AdityaVaidya1 <adityavaidya375@gmail.com>
2024-04-30 17:24:44 +0530

    Implemented compaction

--- BloomFilter.py
+++ BloomFilter.py
@@ -6,19 +6,22 @@ class BloomFilter:
     def __init__(self, capacity, error_rate):
         self.capacity = capacity
         self.error_rate = error_rate
-        self.bit_array = bitarray(self.capacity)
+
+        number_of_bits = int(-(self.capacity)*math.log(error_rate)/((math.log(2))**2))
+
+        self.bit_array = bitarray(number_of_bits)
         self.bit_array.setall(0)
 
-        self.num_hash_functions = int(-(capacity * (error_rate) / (math.log(2) ** 2)))
+        self.num_hash_functions = int(number_of_bits*math.log(2)/self.capacity)
 
     def add(self, item):
         for i in range(self.num_hash_functions):
-            hash_index = mmh3.hash(item, i) % self.capacity
+            hash_index = mmh3.hash(str(item), i) % len(self.bit_array)
             self.bit_array[hash_index] = True
 
     def __contains__(self, item):
         for i in range(self.num_hash_functions):
-            hash_index = mmh3.hash(item, i) % self.capacity
+            hash_index = mmh3.hash(str(item), i) % len(self.bit_array)
             if not self.bit_array[hash_index]:
                 return False
         return True
--- Compaction.py
+++ Compaction.py
@@ -1,14 +1,19 @@
 import os
 import shutil
 import csv
+import json
+import heapq 
+from SSTable import SSTable
+
 class CompactionManager:
-    def __init__(self, parent_folder, subfolders, threshold):
+    def __init__(self, parent_folder, subfolders, threshold, sstable_map):
         self.parent_folder = parent_folder
         self.subfolders = subfolders
         self.threshold = threshold
         self.folder_paths = {}
+        self.merge_no = 0
+        self.sstable_map = sstable_map
 
-    def create_folders(self):
         if not os.path.exists(self.parent_folder):
             os.makedirs(self.parent_folder)
         for folder in self.subfolders:
@@ -23,6 +28,9 @@ class CompactionManager:
             file_count = len([name for name in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, name))])
             if file_count >= self.threshold:
                 self.compact_and_move(folder_name)
+                return True
+        
+        return False
 
     def compact_and_move(self, folder_name):
         folder_path = self.folder_paths.get(folder_name)
@@ -31,64 +39,33 @@ class CompactionManager:
             next_folder_name = self.subfolders[next_folder_index]
             next_folder_path = self.folder_paths.get(next_folder_name)
 
-        # List to store contents of all CSV files
-        all_csv_contents = []
-
-        # Iterate through CSV files in the current folder
-        for filename in os.listdir(folder_path):
-            if filename.endswith(".csv"):
-                file_path = os.path.join(folder_path, filename)
-                with open(file_path, 'r') as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        all_csv_contents.append(row)
-
-                # Remove the old CSV file after processing
-                os.remove(file_path)
-
-        all_csv_contents.sort(key=lambda x: int(x['key']))
+        # List to store contents of all JSON files
+        all_files_contents = []
+        
+        
+        sstable_list = self.sstable_map[folder_name]
+        for sstable in sstable_list:
+            
+            filepath = sstable.filepath
+            with open(filepath, "r") as file:
+                data_dict = json.load(file)
+                all_files_contents.append(data_dict.items())
+                
+            os.remove(filepath)
+        
+        self.merge_no += 1
+        
+        compacted_file_name = 'merged' + str(self.merge_no)
+        merged_sorted_list = list(heapq.merge(*all_files_contents))
+        sstable_obj = SSTable(folder_path=next_folder_path + "/", filename=compacted_file_name , key_value_list=merged_sorted_list, capacity=len(merged_sorted_list))
+        #all_files_contents.sort(key=lambda x: int(x['key']))
+        
+        self.sstable_map[folder_name] = []
+        self.sstable_map[next_folder_name].append(sstable_obj)
         
         # Write the merged contents to a new CSV file in the next folder
-        merged_csv_path = os.path.join(next_folder_path, 'merged.csv')
-        with open(merged_csv_path, 'w', newline='') as csvfile:
-            fieldnames = ['key', 'value']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in all_csv_contents:
-                writer.writerow(row)
-
-        print(f"CSV files merged and moved from {folder_name} to {next_folder_name}.")
-
-     
-    def create_csv_files(self, folder_path=None):
-        if folder_path is None:
-            folder_path = self.folder_paths["folder1"]  # Default to folder1 if no folder_path is provided
-        for i in range(4):  # Creating 4 files in the specified folder
-            file_path = os.path.join(folder_path, f"file{i}.csv")
-            with open(file_path, 'w', newline='') as csvfile:
-                fieldnames = ['key', 'value']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
-                for j in range(1, 11):  # 10 key-value pairs
-                    writer.writerow({'key': j, 'value': f"value_{j}"})
-
-# Example usage 
-# Functions are working as expected
-# Working on linking them with each other so that compaction is achieved.
-
-'''if __name__ == "__main__":
-    parent_folder = "disk"
-    subfolders = [f"folder{i}" for i in range(1, 6)]
-    threshold = 3
-
-    compaction_manager = CompactionManager(parent_folder, subfolders, threshold)
-    compaction_manager.create_folders()
-
-    # Simulate adding CSV files with key-value pairs to folders
-    for folder_name in subfolders:
-        folder_path = compaction_manager.folder_paths[folder_name]
-        compaction_manager.create_csv_files(folder_path)
-
-    # Check threshold and compact if necessary
-    for folder_name in subfolders:
-        compaction_manager.check_threshold_and_compact(folder_name)'''
+       
+    def manage_compact(self):
+        for folder_name in self.subfolders:
+            if not self.check_threshold_and_compact(folder_name):
+                break
