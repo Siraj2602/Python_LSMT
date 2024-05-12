bd7e7fb90ec7cc8725100798b29ed6451a1f2ba8
AdityaVaidya1 <adityavaidya375@gmail.com>
2024-05-12 20:44:36 +0530

    Added Find Logic, Deletion Logic, Changed RedBlackTree Implementation, Added Free Logic

--- LSMTrees.py
+++ LSMTrees.py
@@ -8,19 +8,20 @@ import json
 import time
 from tqdm import tqdm
 import os
+import shutil
 
 def merge_sorted_lists(sorted_lists):
     return list(merge(*sorted_lists))
 
 class LSMTree:
-    def __init__(self, capacity=300000):
+    def __init__(self, capacity):
         self.memory = RedBlackTree()
         self.disk = []
         self.capacity = capacity
         self.sstable_no = 0
         self.tombstone = ""
 
-        parent_folder = "disk"
+        self.parent_folder = "disk"
         subfolders = [f"folder{i}" for i in range(1, 6)]
         threshold = 3
 
@@ -28,63 +29,85 @@ class LSMTree:
         for folder in subfolders:
             self.sstable_map[folder] = []
 
-        self.compaction_manager = CompactionManager(parent_folder, subfolders, threshold, self.sstable_map)
-
-    def construct(self, data):
-        self.disk.append(sorted(data))
+        self.compaction_manager = CompactionManager(self.parent_folder, subfolders, threshold, self.sstable_map)
 
     def free(self):
-        self.disk = []
+        self.memory = RedBlackTree()
+        
+        folder_path = self.parent_folder
+        for item in os.listdir(folder_path):
+            item_path = os.path.join(folder_path, item)
+            # If the item is a file, delete it
+            if os.path.isfile(item_path):
+                os.unlink(item_path)
+            # If the item is a folder, delete it recursively
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+                
+        self.__init__(self.capacity)
 
     def insert(self, element):
-        key, value = element
+        #key, value = element
         if self.memory.length < self.capacity:
-            self.memory.insert(key, value)
+            self.memory.insert(element)
         else:
             self.flush_memory_to_disk()
             self.memory = RedBlackTree()
-            self.memory.insert(key,value)
+            self.memory.insert(element)
+    
+    def construct(self, key_val_list):
+        for key, value in key_val_list:
+            self.insert((key, value))
 
-    def find(self, key):
-        if self.memory.search(key):
-              return True
+    def _find_helper(self, sstable_obj: SSTable, key):
         
-        for folder in self.sstable_map:
-            for sstable_obj in self.sstable_map[folder]:
-                if sstable_obj.find(key):
-                    return True        
-        return False
-
-    def _delete_key_helper(self, sstable_obj: SSTable, key):
-
         file_path = sstable_obj.filepath
 
         # Read the original JSON file line by line and update the specified key-value pair
-        with open(file_path, 'r+') as file:
-            
-            key_start_index = None
-            key_end_index = None
+        with open(file_path, 'r') as file:
             segment_offset = ""
             for start_index, end_index in sstable_obj.index_table:
                 if start_index <= key <= end_index:
                     segment_offset = sstable_obj.index_table[(start_index, end_index)]
-                    key_start_index = start_index
-                    key_end_index = end_index
                     break
-
+                
+            if not segment_offset :
+                return False
             file.seek(int(segment_offset))
-
             segment_data = json.loads(file.readline())
+            
+            if str(key) in  segment_data:
+                return segment_data[str(key)]
+            else:
+                return False
+    
+    def find(self, key):
+        node = self.memory.search(key)
+        if node:
+            return node.value
+        
+        
 
-            segment_data[key] = self.tombstone
+        for folder in self.sstable_map:
+            for sstable_obj in self.sstable_map[folder]:
+                if sstable_obj.find(key):
+                    val = self._find_helper(sstable_obj, key)
+                    if val == False:
+                        self.fp_count += 1
+                    else:
+                        return val       
+        return False
 
-            segment_data = [(int(key), value) for key, value in segment_data.items()]
+    def _delete_key_helper(self, sstable_obj, key):
+        # Perform deletion logic for a key within an SSTable
+        # Here, we need to check delete_meta file associated with the SSTable
 
-            segment_data = dict(sorted(segment_data, key=lambda x: x[0]))
+        delete_meta_file_path = sstable_obj.delete_meta_path
 
-            file.seek(sstable_obj.index_table[(key_start_index, key_end_index)])
 
-            json.dump(segment_data, file)
+        with open(delete_meta_file_path, 'a') as f:
+            f.write(str(key) + "\n")
+
 
     def delete(self, key):
         if self.memory.search(key):
@@ -101,7 +124,11 @@ class LSMTree:
 
         if not found:
             print("Key not found")
-    
+        else:
+            #print("key is deleted!!")
+            pass
+
+        
     def _range_query_sstables(self, start, end):
         result = []
         for folder in self.sstable_map:
@@ -154,54 +181,64 @@ class LSMTree:
         #    self.compact()
         self.compaction_manager.manage_compact()
 
-    def compact(self):
-        segments_to_merge = self.disk[:self.merge_threshold]
-        merged_segment = merge_sorted_lists(segments_to_merge)
 
-        self.disk = self.disk[self.merge_threshold:]
-        self.disk.append(merged_segment)
-
+number_of_elements = 1000000
 
 # Example usage:
-lsm_tree = LSMTree()
+lsm_tree = LSMTree(capacity=int(0.05*number_of_elements))
 
 # Construct LSM tree
-random_keys = random.sample(range(1, 10000001), 10000000)
+random_keys = random.sample(range(1, number_of_elements + 1), number_of_elements)
 #random_keys = [str(i) for i in random_keys]
-random_values = random.sample(range(1, 10000001), 10000000)
+random_values = random.sample(range(1, number_of_elements + 1), number_of_elements)
 random_elements = list(zip(random_keys, random_values))
 
 start = time.time()
-for element in tqdm(random_elements):
+
+lsm_tree.construct(random_elements)
+
+end = time.time()
+
+print("Time taken for inserts :", end-start)
+
+"""
+start = time.time()
+
+for key, val in tqdm(random_elements[250000:]):
+    lsm_tree.find(key)
+
+end = time.time()
+
+print("Time taken for finds :", end-start)
+
+print("\n False positive count for 50000 is , ", lsm_tree.fp_count)
+"""
+start = time.time()
+
+result = lsm_tree.range_query(start=1, end=int(number_of_elements * 0.5))
+
+end = time.time()
+
+print("Time taken for Range Query : ", end - start)
+
+range_query_time = end - start
+
+start = time.time()
+
+for key, val in tqdm(random_elements[: int(number_of_elements * 0.5)]):
+    lsm_tree.delete(key)
+
+end = time.time()
+
+print("Time taken for Deletion : ", end - start)
+
+for element in tqdm(random_elements[: int(number_of_elements*0.5)]):
     lsm_tree.insert(element)
-
-end = time.time()
-
-print("Time taken for 10Million inserts ", end-start)
-print("LSM Tree SSTable Dictionary", lsm_tree.sstable_map)
-
+    
 start = time.time()
 
-result = lsm_tree.range_query(start=1, end=50000)
+lsm_tree.free()
 
 end = time.time()
 
-print("Range Query result, ", len(result))
-print("Start element :", result[0])
-print("End element :", result[-1])
-print("Range Query Time : ", end - start)
-
-"""
-for i in random.sample(range(10, 60), 40):
-    lsm_tree.delete(random_elements[i][0])
-
-
-start = time.time()
-
-for i in tqdm(range(len(random_elements))):
-    lsm_tree.delete(random_elements[i][0])
-
-end = time.time()
-print("Time taken for 50000 Deletes ", end-start)
-
-"""
+print("Time taken for Free : ", end - start)
--- RedBlackTrees.py
+++ RedBlackTrees.py
@@ -1,228 +1,258 @@
+from collections import deque
+
+
+BLACK = True
+RED = False
+
+
 class Node:
-    def __init__(self, key, value, color='RED'):
+    def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.p = None # parent
+        self.color = RED
         self.left = None
         self.right = None
-        self.parent = None
-        self.color = color
+
+    def print_color(self):
+        if self.color == BLACK:
+            return '(b)'
+        return '(r)'
+
 
 class RedBlackTree:
     def __init__(self):
-        self.root = None
+        self.NIL = Node(-1, None) # arbitrary value
+        self.NIL.color = BLACK
+        self.NIL.left = None
+        self.NIL.right = None
+        self.root = self.NIL
         self.length = 0
 
-    def insert(self, key, value):
-        node = Node(key, value)
-        self._insert_helper(node)
+    # O(1)
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left 
+
+        if y.left != self.NIL:
+            y.left.p = x
+        
+        y.p = x.p 
+
+        if x.p is None:
+            self.root = y
+        elif x == x.p.left:
+            x.p.left = y
+        else:
+            x.p.right = y 
+
+        y.left = x 
+        x.p = y
+
+    # O(1)
+    def right_rotate(self, x):
+        y = x.left 
+        x.left = y.right 
+
+        if y.right != self.NIL:
+            y.right.p = x
+
+        y.p = x.p 
+
+        if x.p is None:
+            self.root = y 
+        elif x == x.p.right:
+            x.p.right = y 
+        else:
+            x.p.left = y 
+
+        y.right = x 
+        x.p = y
+
+    # O(logn) total
+    def insert(self, element):
+        key, value = element
+        z = Node(key, value)
         self.length += 1
+        z.left = self.NIL
+        z.right = self.NIL
 
-    def _insert_helper(self, node):
-        parent = None
-        current = self.root
-
-        while current is not None:
-            parent = current
-            if node.key < current.key:
-                current = current.left
+        y = None 
+        x = self.root
+        
+        while x != self.NIL:
+            y = x
+            if z.key < x.key:
+                x = x.left 
             else:
-                current = current.right
-
-        node.parent = parent
-        if parent is None:
-            self.root = node
-        elif node.key < parent.key:
-            parent.left = node
+                x = x.right 
+        
+        z.p = y 
+        if y == None:
+            self.root = z 
+        elif z.key < y.key: 
+            y.left = z 
         else:
-            parent.right = node
+            y.right = z
 
-        node.left = None
-        node.right = None
-        node.color = 'RED'
-        self._insert_fixup(node)
+        self.insert_fixup(z)
 
-    def _insert_fixup(self, node):
-        while node.parent is not None and node.parent.color == 'RED':
-            if node.parent == node.parent.parent.left:
-                uncle = node.parent.parent.right
-                if uncle is not None and uncle.color == 'RED':
-                    node.parent.color = 'BLACK'
-                    uncle.color = 'BLACK'
-                    node.parent.parent.color = 'RED'
-                    node = node.parent.parent
+    # O(logn)
+    def insert_fixup(self, z):
+        while z.p and z.p.color == RED:
+            if z.p == z.p.p.left:
+                y = z.p.p.right 
+                if y.color == RED:
+                    z.p.color = BLACK
+                    y.color = BLACK 
+                    z.p.p.color = RED
+                    z = z.p.p
                 else:
-                    if node == node.parent.right:
-                        node = node.parent
-                        self._left_rotate(node)
-                    node.parent.color = 'BLACK'
-                    node.parent.parent.color = 'RED'
-                    self._right_rotate(node.parent.parent)
+                    if z == z.p.right:
+                        z = z.p 
+                        self.left_rotate(z)
+                    z.p.color = BLACK
+                    z.p.p.color = RED 
+                    self.right_rotate(z.p.p)
             else:
-                uncle = node.parent.parent.left
-                if uncle is not None and uncle.color == 'RED':
-                    node.parent.color = 'BLACK'
-                    uncle.color = 'BLACK'
-                    node.parent.parent.color = 'RED'
-                    node = node.parent.parent
+                y = z.p.p.left 
+                if y.color == RED:
+                    z.p.color = BLACK
+                    y.color = BLACK
+                    z.p.p.color = RED
+                    z = z.p.p
                 else:
-                    if node == node.parent.left:
-                        node = node.parent
-                        self._right_rotate(node)
-                    node.parent.color = 'BLACK'
-                    node.parent.parent.color = 'RED'
-                    self._left_rotate(node.parent.parent)
+                    if z == z.p.left:
+                        z = z.p 
+                        self.right_rotate(z)
+                    z.p.color = BLACK
+                    z.p.p.color = RED 
+                    self.left_rotate(z.p.p)
+            if z == self.root:
+                break
+        self.root.color = BLACK
 
-        self.root.color = 'BLACK'
-
-    def _left_rotate(self, node):
-        right_child = node.right
-        node.right = right_child.left
-        if right_child.left is not None:
-            right_child.left.parent = node
-        right_child.parent = node.parent
-        if node.parent is None:
-            self.root = right_child
-        elif node == node.parent.left:
-            node.parent.left = right_child
-        else:
-            node.parent.right = right_child
-        right_child.left = node
-        node.parent = right_child
-
-    def _right_rotate(self, node):
-        left_child = node.left
-        node.left = left_child.right
-        if left_child.right is not None:
-            left_child.right.parent = node
-        left_child.parent = node.parent
-        if node.parent is None:
-            self.root = left_child
-        elif node == node.parent.right:
-            node.parent.right = left_child
-        else:
-            node.parent.left = left_child
-        left_child.right = node
-        node.parent = left_child
-
+    # O(logn) total
     def delete(self, key):
-        node = self.search(key)
-        if node is None:
-            return
-        self._delete_node(node)
-        self.length -= 1
+        z = self.search(key)
 
-    def _delete_node(self, node):
-        original_color = node.color
-        if node.left is None:
-            child = node.right
-            self._transplant(node, node.right)
-        elif node.right is None:
-            child = node.left
-            self._transplant(node, node.left)
+        if z == self.NIL:
+            return "Key not found!"
+
+        y = z
+        y_orig_color = y.color 
+        
+        # case 1
+        if z.left == self.NIL:
+            x = z.right 
+            self.transplant(z, z.right)
+        # case 2
+        elif z.right == self.NIL:
+            x = z.left
+            self.transplant(z, z.left)
+        # case 3
         else:
-            successor = self._minimum(node.right)
-            original_color = successor.color
-            child = successor.right
-            if successor.parent == node:
-                child.parent = successor
+            y = self.minimum(z.right)
+            y_orig_color = y.color
+            x = y.right 
+            
+            if y.p == z:
+                x.p = y
             else:
-                self._transplant(successor, successor.right)
-                successor.right = node.right
-                successor.right.parent = successor
-            self._transplant(node, successor)
-            successor.left = node.left
-            successor.left.parent = successor
-            successor.color = node.color
-        if original_color == 'BLACK':
-            self._delete_fixup(child)
+                self.transplant(y, y.right)
+                y.right = z.right
+                y.right.p = y
+            
+            self.transplant(z, y)
+            y.left = z.left 
+            y.left.p = y 
+            y.color = z.color 
+        
+        if y_orig_color == BLACK:
+            self.delete_fixup(x)
 
-    def _transplant(self, u, v):
-        if u.parent is None:
+    # O(logn)
+    def delete_fixup(self, x):
+        while x != self.root and x.color == BLACK:
+            if x == x.p.left:
+                w = x.p.right
+                # type 1
+                if w.color == RED:
+                    w.color = BLACK
+                    x.p.color = RED
+                    self.left_rotate(x.p)
+                    w = x.p.right
+                # type 2
+                if w.left.color == BLACK and w.right.color == BLACK:
+                    w.color = RED 
+                    x = x.p 
+                else:
+                    # type 3
+                    if w.right.color == BLACK:
+                        w.left.color = BLACK
+                        w.color = RED
+                        self.right_rotate(w)
+                        w = x.p.right
+                    # type 4
+                    w.color = x.p.color 
+                    x.p.color = BLACK 
+                    w.right.color = BLACK 
+                    self.left_rotate(x.p)
+                    x = self.root
+            else:
+                w = x.p.left
+                # type 1
+                if w.color == RED:
+                    w.color = BLACK
+                    x.p.color = RED
+                    self.right_rotate(x.p)
+                    w = x.p.left
+                # type 2
+                if w.right.color == BLACK and w.left.color == BLACK:
+                    w.color = RED 
+                    x = x.p 
+                else:
+                    # type 3
+                    if w.left.color == BLACK:
+                        w.right.color = BLACK
+                        w.color = RED
+                        self.left_rotate(w)
+                        w = x.p.left
+                    # type 4
+                    w.color = x.p.color 
+                    x.p.color = BLACK 
+                    w.left.color = BLACK 
+                    self.right_rotate(x.p)
+                    x = self.root
+        x.color = BLACK
+
+    # O(1)
+    def transplant(self, u, v):
+        if u.p == None:
             self.root = v
-        elif u == u.parent.left:
-            u.parent.left = v
+        elif u == u.p.left:
+            u.p.left = v 
         else:
-            u.parent.right = v
-        if v is not None:
-            v.parent = u.parent
+            u.p.right = v
+        v.p = u.p 
 
-    def _delete_fixup(self, node):
-        while node != self.root and node.color == 'BLACK':
-            if node == node.parent.left:
-                sibling = node.parent.right
-                if sibling.color == 'RED':
-                    sibling.color = 'BLACK'
-                    node.parent.color = 'RED'
-                    self._left_rotate(node.parent)
-                    sibling = node.parent.right
-                if (sibling.left.color == 'BLACK' and
-                    sibling.right.color == 'BLACK'):
-                    sibling.color = 'RED'
-                    node = node.parent
-                else:
-                    if sibling.right.color == 'BLACK':
-                        sibling.left.color = 'BLACK'
-                        sibling.color = 'RED'
-                        self._right_rotate(sibling)
-                        sibling = node.parent.right
-                    sibling.color = node.parent.color
-                    node.parent.color = 'BLACK'
-                    sibling.right.color = 'BLACK'
-                    self._left_rotate(node.parent)
-                    node = self.root
-            else:
-                sibling = node.parent.left
-                if sibling.color == 'RED':
-                    sibling.color = 'BLACK'
-                    node.parent.color = 'RED'
-                    self._right_rotate(node.parent)
-                    sibling = node.parent.left
-                if (sibling.right.color == 'BLACK' and
-                    sibling.left.color == 'BLACK'):
-                    sibling.color = 'RED'
-                    node = node.parent
-                else:
-                    if sibling.left.color == 'BLACK':
-                        sibling.right.color = 'BLACK'
-                        sibling.color = 'RED'
-                        self._left_rotate(sibling)
-                        sibling = node.parent.left
-                    sibling.color = node.parent.color
-                    node.parent.color = 'BLACK'
-                    sibling.left.color = 'BLACK'
-                    self._right_rotate(node.parent)
-                    node = self.root
-        node.color = 'BLACK'
+    # O(h) = O(logn) for RB trees
+    def minimum(self, x):
+        while x.left != self.NIL:
+            x = x.left
+        return x
 
+    # O(h) = O(logn) for RB trees
     def search(self, key):
-        current = self.root
-        while current is not None:
-            if key == current.key:
-                return current
-            elif key < current.key:
-                current = current.left
+        x = self.root
+        while x != self.NIL and key != x.key:
+            if key < x.key:
+                x = x.left
             else:
-                current = current.right
-        return None
-
-    def _minimum(self, node):
-        while node.left is not None:
-            node = node.left
-        return node
-
-    def _inorder_helper(self, node, result):
-        if node is None:
-            return
-        if node is not None:
-            self._inorder_helper(node.left, result)
-            result.append((node.key, node.value))
-            self._inorder_helper(node.right, result)
-
-
-    def inorder_traversal(self):
-        result = []
-        self._inorder_helper(self.root, result)
-        return result
+                x = x.right
+        if x == self.NIL:
+            return False
+        return x
 
     def range_query(self, start_key, end_key):
         result = []
@@ -245,32 +275,14 @@ class RedBlackTree:
         elif node.key > end_key:
             self._range_query_helper(node.left, start_key, end_key, result)
 
+    def _inorder_helper(self, node, result):
+        if node == self.NIL:
+            return
+        self._inorder_helper(node.left, result)
+        result.append((node.key, node.value))
+        self._inorder_helper(node.right, result)
 
-# Example usage:
-"""
-if __name__ == "__main__":
-    rb_tree = RedBlackTree()
-    keys = [10, 5, 15, 3, 7, 12, 18, 1, 4, 6, 8, 11, 13, 17, 20]
-
-    for key in keys:
-        rb_tree.insert(key)
-
-    print("Length of Tree : ", rb_tree.length)
-    print("Inorder traversal of the Red-Black Tree:")
-    rb_tree.inorder_traversal()
-
-    search_key = 10
-    result = rb_tree.search(search_key)
-    if result is not None:
-        print(f"Node with key {search_key} found.")
-    else:
-        print(f"Node with key {search_key} not found.")
-
-    delete_key = 15
-    rb_tree.delete(delete_key)
-    print(f"Node with key {delete_key} deleted.")
-    print("Length of tree is ", rb_tree.length)
-
-    print("Inorder traversal after deletion:")
-    rb_tree.inorder_traversal()
-"""
+    def inorder_traversal(self):
+        result = []
+        self._inorder_helper(self.root, result)
+        return result
